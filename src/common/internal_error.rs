4dbe74d401b6418c1100a88ad998ab8e35bd7387
opannapo <taufan.alfazri@gmail.com>
2025-01-19 21:27:56 +0700

    Proper internal code as string

--- src/common/internal_error.rs
+++ src/common/internal_error.rs
@@ -6,11 +6,12 @@ use std::fmt;
 pub struct InternalError {
     pub http_status_code: StatusCode,
     pub message: String,
+    pub code: String,
 }
 
 impl fmt::Display for InternalError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Error : {}", self.message)
+        write!(f, "{} : {}", self.code, self.message)
     }
 }
 
@@ -19,6 +20,7 @@ impl Error for InternalError {}
 impl InternalError {
     pub fn db_exec(message: &str) -> Self {
         InternalError {
+            code: "DB_EXEC".to_string(),
             http_status_code: StatusCode::INTERNAL_SERVER_ERROR,
             message: message.to_string(),
         }
